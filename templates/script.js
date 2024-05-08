ddf9c18fa92e86e2cd71ac76c26171fd0073c850
marcello2k18 <128180508+marcello2k18@users.noreply.github.com>
2024-05-08 11:02:03 +0700

    Add files via upload

---
+++ templates/script.js
@@ -0,0 +1,7 @@
+<script src="https://appsrv1-147a1.kxcdn.com/soft-ui-dashboard-flask-v1.0.8/js/core/popper.min.js"></script>
+<script src="https://appsrv1-147a1.kxcdn.com/soft-ui-dashboard-flask-v1.0.8/js/core/bootstrap.min.js"></script>
+<script src="https://appsrv1-147a1.kxcdn.com/soft-ui-dashboard-flask-v1.0.8/js/plugins/perfect-scrollbar.min.js"></script>
+<script src="https://appsrv1-147a1.kxcdn.com/soft-ui-dashboard-flask-v1.0.8/js/plugins/smooth-scrollbar.min.js"></script>
+<script src="https://appsrv1-147a1.kxcdn.com/soft-ui-dashboard-flask-v1.0.8/js/dark-mode-handler.js"></script>
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+<script src="https://appsrv1-147a1.kxcdn.com/soft-ui-dashboard-flask-v1.0.8/js/soft-ui-dashboard.min.js?v=1.0.6"></script>
